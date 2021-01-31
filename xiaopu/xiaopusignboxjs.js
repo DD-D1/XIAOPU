cbaeac9269d87f2a83f679238910eb5529d0c9f9
DD <59794403+DD-D1@users.noreply.github.com>
2021-02-01 01:33:28 +0800

    Add files via upload

---
+++ xiaopu/xiaopusignboxjs.js
@@ -0,0 +1,38 @@
+{
+"id": "DD.app.sub",
+"name": "笑谱APP",
+"author": "@DD",
+"icon": "http://elf.static.maibaapp.com/content/ios-countdown/11891342/avatar/11591339654571.png",
+"repo": "https://github.com/DD-D1/XIAOPU",
+"apps": [{
+"id": "xiaopusign",
+"name": "笑谱APP",
+"keys": ["iboxpayheader","tokenbody","iboxpayheader2","tokenbody","iboxpayheader3","tokenbody3","iboxpayheader4","tokenbody4","iboxpayheader5","tokenbody5","iboxpayheader6","tokenbody6","iboxpayheader7","tokenbody7","iboxpayheader8","tokenbody8","iboxpayheader9","tokenbody9","iboxpayheader10","tokenbody10"],
+"author": "@dd",
+"settings": [{
+"id": "xiaopuSuffix",
+"name": "当前账号",
+"val": "1",
+"type": "number",
+"desc": "当前获取cookie记录的账号，多账号例如：1、2、3、目前最多10个账号"
+},
+{
+"id": "xiaopuCount",
+"name": "账号个数",
+"val": "1",
+"type": "number",
+"desc": "指定几个账号,跑几个账号的任务,最多跑10个账号"
+},
+{
+"id": "noNolog",
+"name": "系统通知",
+"val": "1",
+"type": "number",
+"desc": "1所有通知 2,11小时通知一次 3,3小时通知一次 4,5小时通知一下"
+}
+],
+"repo": "https://github.com/DD-D1/XIAOPU/blob/main/xiaopu/xiaopusign.js",
+"icons": ["https://raw.githubusercontent.com/DD-D1/XIAOPU/main/xiaopu/F3453B39-F2B3-4B12-8923-4DF6341CC7DD.png","https://raw.githubusercontent.com/DD-D1/XIAOPU/main/xiaopu/F3453B39-F2B3-4B12-8923-4DF6341CC7DD.png"],
+"script": "https://raw.githubusercontent.com/DD-D1/XIAOPU/main/xiaopu/xiaopusign.js"
+}]
+}
